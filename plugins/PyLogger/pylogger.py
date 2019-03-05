3366ea858a8c6b320c5826cb3c0844e16694ca84
Edenskull <maxchamp@live.fr>
2019-03-05 22:40:47 +0100

    Update PyLogger Version

--- plugins/PyLogger/pylogger.py
+++ plugins/PyLogger/pylogger.py
@@ -5,16 +5,18 @@ import configparser
 from time import strftime
 
 
+root = os.path.dirname(os.path.realpath(__file__))
+
 if not os.path.exists('log'):  # Create the folder if it doesn't exists
     os.mkdir('log')
 
 config = configparser.ConfigParser()
-config.read('logger.ini')
+config.read(os.path.join(root, 'logger.ini'))
 
-filename = "log/{}-{}.log".format(config['Appname'], strftime("%Y_%m_%d_%H_%M_%S"))
+filename = "log/{}-{}.log".format(config['PYLOGGER']['Appname'], strftime("%Y_%m_%d_%H_%M_%S"))
 
 pylogger = logging.getLogger(__name__)
-pylogger.setLevel(logging.getLevelName(config['Level']))
+pylogger.setLevel(logging.getLevelName(config['PYLOGGER']['Level']))
 
 handler = logging.FileHandler(filename, 'w', 'utf-8')
 formatter = logging.Formatter(
